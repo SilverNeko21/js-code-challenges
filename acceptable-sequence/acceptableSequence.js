ec4de837e5c60df2274d3bbf43db01c28df50f33
SilverNeko21 <larong2122@gmail.com>
2018-08-22 14:40:25 -1000

    oof

--- acceptable-sequence/acceptableSequence.js
+++ acceptable-sequence/acceptableSequence.js
@@ -1,3 +1,13 @@
-module.exports = function acceptableSequence(str) {
+var i;
+var n;
+var letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-}
+module.exports = function acceptableSequence(str) {
+    for(i = 0; i < str.length; i++){
+       if (str.indexof(letters) !== "-1"){
+           
+       }
+    }
+
+    console.log('str', str)
+};
